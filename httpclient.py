84e95ce758380ad9295150c2ec01cb2b898fd8ba
loktung <47838124+loktung@users.noreply.github.com>
2019-06-20 14:25:38 -0400

    Update httpclient.py

--- httpclient.py
+++ httpclient.py
@@ -10,12 +10,5 @@ httprequest=input('Input HTTP request <method> <URL> <version>:')
 clientSocket.send(httprequest.encode())#send to request to the proxy 
 #below decode and print the response receive from proxy
 print(clientSocket.recv(1024).decode())
-print(clientSocket.recv(1024).decode())
-print(clientSocket.recv(1024).decode())
-print(clientSocket.recv(1024).decode())
-print(clientSocket.recv(1024).decode())
-print(clientSocket.recv(1024).decode())
-print(clientSocket.recv(1024).decode())
-print(clientSocket.recv(1024).decode())
 #it should print the status
 clientSocket.close()
